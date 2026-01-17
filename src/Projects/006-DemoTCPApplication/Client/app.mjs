5d23c11e2b4d69ec0e0445cd8c388f2708ca2698
Oğuz Karan <oguzkaran@csystem.org>
2026-01-17 14:34:41 +0300

    update

--- src/Projects/006-DemoTCPApplication/Client/app.mjs
+++ src/Projects/006-DemoTCPApplication/Client/app.mjs
@@ -1,21 +1,39 @@
 import net from "node:net"
 
-
-const connectionCallback = client => {
-    console.log("Client connected")
+const upperClientConnectionCallback = client => {
+    console.log("Client connected to upper server")
 
     client.write(process.argv[2])
     client.end()
 }
 
+const randomNumberClientConnectionCallback = client => {
+    console.log("Client connected to random number server")
 
+    const buf = Buffer.alloc(8)
 
+    buf.writeInt32BE(parseInt(process.argv[3]), 0)
+    buf.writeInt32BE(parseInt(process.argv[4]), 4)
 
-const main = () => {
-    const client = net.createConnection({port: 50500, host: "127.0.0.1"}, () => connectionCallback(client))
-    client.on("data", data => console.log("Data received from server:", data.toString()))
-    client.on("end", () => console.log("Disconnected from server"))
+    client.write(buf)
+    client.end()
 }
 
+const randomNumberDataCallback = d => {
+    console.log("Data received from random server:", d.readInt32BE(0))
+}
+
+const main = () => {
+    //Command line arguments check
+    const upperClient = net.createConnection({port: 50500, host: "127.0.0.1"}, () => upperClientConnectionCallback(upperClient))
+    const randomNumberClient = net.createConnection({port: 50501, host: "127.0.0.1"}, () => randomNumberClientConnectionCallback(randomNumberClient))
+
+    upperClient.on("data", data => console.log("Data received from upper server:", data.toString()))
+    upperClient.on("end", () => console.log("Disconnected from upper server"))
+    randomNumberClient.on("data", randomNumberDataCallback)
+    randomNumberClient.on("end", () => console.log("Disconnected from random number server"))
+
+
+}
 
 main()
